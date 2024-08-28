8ff573899179ecab18abaa4565861fdb28328b3e
Takiul ISlam Sumon <takiulislam20@gmail.com>
2024-08-29 03:02:29 +0600

    Shaped

--- lib/app_body.dart
+++ lib/app_body.dart
@@ -9,7 +9,7 @@ class homepage extends StatefulWidget {
 
 class _homepageState extends State<homepage> {
   TextEditingController search_product = TextEditingController();
-  final filters = ['All', 'Adidas', 'Nike', 'Bata'];
+  final List<String> filters = <String>['All', 'Adidas', 'Nike', 'Bata'];
 
   @override
   Widget build(BuildContext context) {
@@ -17,34 +17,51 @@ class _homepageState extends State<homepage> {
       appBar: AppBar(),
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Row(
-            //   children: [
-            //     const Text(
-            //       'Shoes\nCollection',
-            //       style: TextStyle(
-            //           // fontFamily: 'Pelita',
-            //           fontWeight: FontWeight.bold,
-            //           fontSize: 34),
-            //     ),
-            //     Expanded(
-            //       child: TextField(
-            //         controller: search_product,
-            //         decoration: const InputDecoration(
-            //             border: OutlineInputBorder(
-            //                 borderRadius: BorderRadius.horizontal(
-            //                     left: Radius.circular(25))),
-            //             hintText: 'Search',
-            //             prefixIcon: Icon(Icons.search)),
-            //       ),
-            //     )
-            //   ],
-            // ),
-            ListView.builder(
-              itemCount: 4,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(leading: Text(filters[index]),);
-              },
+            Row(
+              children: [
+                const Text(
+                  'Shoes\nCollection',
+                  style: TextStyle(
+                      // fontFamily: 'Pelita',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 34),
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: search_product,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.horizontal(
+                                left: Radius.circular(25))),
+                        hintText: 'Search',
+                        prefixIcon: Icon(Icons.search)),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 100,
+              width: double.infinity,
+              child: ListView.builder(
+                itemCount: filters.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  final filter = filters[index];
+                  return Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Text(filter),
+                      foregroundDecoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                      color: Colors.black45,
+                      padding: EdgeInsets.all(10),
+                    ),
+                  );
+                },
+              ),
             )
           ],
         ),
