732c5409f9b39bff6301894988a640baa8677b29
Takiul ISlam Sumon <takiulislam20@gmail.com>
2024-09-12 07:13:37 +0600

    List View

--- lib/product_Info.dart
+++ lib/product_Info.dart
@@ -1,40 +1,74 @@
 import 'package:flutter/material.dart';
 
-
 class Product_Info extends StatelessWidget {
   final String title;
   final String price;
   final String image;
-  const Product_Info({super.key, required this.title,required this.price,required this.image});
+   Product_Info(
+      {super.key,
+      required this.title,
+      required this.price,
+      required this.image});
+  final Shoe_size = [
+    '9','10','11','12'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Details",style: Theme.of(context).textTheme.titleMedium,),
-      centerTitle: true,),
+      appBar: AppBar(
+        title: Text(
+          "Details",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        centerTitle: true,
+      ),
       body: Center(
         child: Container(
-          
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,  children: [
-            Text('$title',style: Theme.of(context).textTheme.titleLarge, ),
-            SizedBox(height: 80),
-            Image(image: AssetImage(image)),
-            Expanded(
-              
-              child: Container(
-                decoration: BoxDecoration(borderRadius: BorderRadius.horizontal(left: Radius.circular(20),right: Radius.circular(20)),
-                color: Colors.amber.shade50,),
-                width: double.infinity,
-              
-              child: Column(children: [
-                 Text('\$$price',style: const TextStyle(fontSize: 28,fontWeight: FontWeight.w800))
-              ],),
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                '$title',
+                style: Theme.of(context).textTheme.titleLarge,
               ),
-            )
-            
-          ],),
-        
+              SizedBox(height: 80),
+              Image(image: AssetImage(image)),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(20), right: Radius.circular(20)),
+                    color: Colors.amber.shade50,
+                  ),
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      Text('\$$price',
+                          style: const TextStyle(
+                              fontSize: 28, fontWeight: FontWeight.w800)),
+                              Expanded(
+                                child: ListView.builder(itemCount: Shoe_size.length,
+                                scrollDirection: Axis.horizontal,
+                                  itemBuilder:(context, index) {
+                                  return  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Chip(
+                                    
+                                      label: Text(Shoe_size[index] )
+                                      
+                                    ),
+                                  );
+                                },
+                                  
+                                                          ),
+                              ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
